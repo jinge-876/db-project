141a9c9f2dc52d051d21430912549f4fc82169d3
jinge-876 <myonly120000@gmail.com>
2025-12-09 08:22:19 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,3 +11,46 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE patient (
+    patientennummer INTEGER PRIMARY KEY,
+    alter INTEGER,
+    name TEXT,
+    krankenkasse TEXT,
+    krankheiten TEXT,
+    ehemalige aufenthalte TEXT,
+    ehemalige medikamente TEXT, 
+    bettnummer INTEGER FOREIGN KEY
+);
+
+CREATE TABLE medizin (
+    fachname TEXT PRIMARY KEY,
+    dosierung TEXT
+);
+
+
+CREATE TABLE arzt (
+    ärztenummer INTEGER PRIMARY KEY,
+    anstellzeit INTEGER,
+    name TEXT,
+    spezialisierung TEXT
+);
+
+CREATE TABLE aktuellerAufenthalt (
+    bettnummer INTEGER PRIMARY KEY,
+    pflegebedarf TEXT,
+    anfangsdatum TEXT
+);
+
+CREATE TABLE nimmt (
+    fachname TEXT PRIMARY KEY,
+    patientennummer INTEGER PRIMARY KEY
+);
+
+CREATE TABLE behandelt (
+    ärztenummer INTEGER PRIMARY KEY,
+    patientennummer INTEGER PRIMARY KEY
+);
+
+
+
